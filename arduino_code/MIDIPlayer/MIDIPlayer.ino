e44089e7583ef9498b13d0d4928f3d32c1479365
Kurt <kpdudek2@gmail.com>
2021-03-14 09:42:45 -0400

    ROS controllable

--- arduino_code/MIDIPlayer/MIDIPlayer.ino
+++ arduino_code/MIDIPlayer/MIDIPlayer.ino
@@ -1,3 +1,7 @@
+#include <ros.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Float32.h>
+
 int pul_pin = 11, dir_pin = 10;
 float pul_per_rev = 800.0;
 unsigned long int t, t_prev = -10000000000;
@@ -6,10 +10,9 @@ float pi = 3.1415926;
 class MIDIElement{
   public:
     char id[20];
-    float step_size, rps;
-    int dir = 1;
+    float step_size, rps, freq;
     int at_goal = 0, stopped = 1;
-    int pul_pin = -1, dir_pin = -1;
+    int pul_pin, dir_pin, dir = 1;
     unsigned long int t = 0, t_prev = -1000, t_diff;
     unsigned int spd = 50, pul_delay = 0, pulse_min = 40, pulse_max = 3000;
     float prev_setpoint = -1.0, setpoint = 6.28, angle = 0.0, error = 0.0, tolerance = 0.05;
@@ -34,47 +37,65 @@ class MIDIElement{
         this->pul_delay = 1.0/(freq*2.0*pul_per_rev/1000000.0);
     }
     void set_midi_tone(int midi_number){
-      float freq = pow(2,(float(midi_number)-69.0)/12.0);
-      this->pul_delay = 1.0/(freq*2.0*pul_per_rev/1000000.0);
+      this->freq = pow(2,(float(midi_number)-69.0)/12.0);
+      this->pul_delay = 1.0/(this->freq*2.0*pul_per_rev/1000000.0);
     }
 };
 
 MIDIElement elements[1] = {MIDIElement(pul_pin,dir_pin, (1.0/pul_per_rev)*2.0*pi, "1")};
 
-int note;
-int idx = 0;
-int scale[] = {60,62,64,65,67,69,71,72,-1,72,71,69,67,65,64,62,60, 9999};
-unsigned long int duration = 200000;
+///////////////////////////////////////////////////////////////////////////////////////////
+// ROS Definitions
+///////////////////////////////////////////////////////////////////////////////////////////
+ros::NodeHandle  nh;
+
+int note = -1;
+std_msgs::Float32 midi_feedback_data;
+ros::Publisher midi_feedback_pub("/midi/feedback", &midi_feedback_data);
+
+void midi_stream_callback(const std_msgs::Int32& data){
+    note = data.data;
+    for (int j=0;j<1;j++){
+      elements[j].set_midi_tone(note);
+      midi_feedback_data.data = elements[j].freq;
+    }
+    midi_feedback_pub.publish(&midi_feedback_data);
+}
+ros::Subscriber<std_msgs::Int32> midi_data_sub("/midi/stream",& midi_stream_callback);
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(pul_pin,OUTPUT);
   pinMode(dir_pin,OUTPUT);
+      
+  // Ros subscribers
+  nh.initNode();
+  nh.subscribe(midi_data_sub);
+  nh.advertise(midi_feedback_pub);
 }
 
 void loop() {
   t = micros();
-  if ((t - t_prev > duration)){
-    note = scale[idx];
-    if (note == 9999){
-      return;
-    }
-    if (note < 0){
-      delayMicroseconds(duration);
-    }
+  if (note > 0){
     for (int j=0;j<1;j++){
-//      elements[j].set_frequency(note);
-      elements[j].set_midi_tone(note);
+      elements[j].update_position();
     }
-    idx++;
-    t_prev = t;
   }
-//  if (idx < (sizeof(scale)/sizeof(int))){
+//  else if ((t - t_prev > duration)){
+//    if (note < 0){
+//      delayMicroseconds(duration);
+//    }
+//    else{
+//      for (int j=0;j<1;j++){
+//        elements[j].set_midi_tone(note);
+//      }
+//      idx++;
+//      t_prev = t;
+//    }
+//  }
+//  else{
 //    for (int j=0;j<1;j++){
 //      elements[j].update_position();
 //    }
 //  }
-  for (int j=0;j<1;j++){
-    elements[j].update_position();
-  }
+  nh.spinOnce();
 }
